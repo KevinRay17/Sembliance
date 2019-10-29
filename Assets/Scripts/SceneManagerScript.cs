45b37f6f865f2b53b2813cf68812a6d337e56b83
Samina Trachier <st3225@nyu.edu>
2019-10-29 16:55:58 -0400

    Created Intermission

---
+++ Assets/Scripts/SceneManagerScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerScript : MonoBehaviour
+{
+
+    public static int nextScene;
+    //nextScene = SceneManager.GetActiveScene().buildIndex +1;
+    //SceneManager.LoadScene(nextScene);
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        DontDestroyOnLoad(this.transform.gameObject);
+        //gets current scene
+        nextScene = SceneManager.GetActiveScene().buildIndex + 1;
+       // SceneManager.LoadScene(nextScene);
+    }
+
+    void Update()
+    {
+
+    }
+
+}
