fb69467f01f9e877a8b1ab97bddbff2e434b9cd5
Adam Goren <amg1090@nyu.edu>
2019-11-02 17:02:28 -0400

    Updated sub and dialogue management

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogueManager : MonoBehaviour
+{
+    public TextMeshProUGUI subtitles;
+    public AudioSource source;
+
+    public GameObject currentTrigger;
+    public int currentLine;
+
+    public List<string> subtitleTexts;
+    public List<AudioClip> voiceOverLines;
+
+    float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        subtitles = GameObject.Find("Subtitles").GetComponent<TextMeshProUGUI>();
+        source = GameObject.Find("Subtitles").GetComponent<AudioSource>();
+
+        subtitles.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!source.isPlaying)
+        {
+            subtitles.text = "";
+        }
+
+        PlayVoice();
+    }
+
+    public IEnumerator playVoiceLines()
+    {
+        subtitles.text = subtitleTexts[currentLine];
+        source.clip = voiceOverLines[currentLine];
+        source.Play();
+        if (currentLine < voiceOverLines.Count)
+        {
+            currentLine++;
+            yield return new WaitForSeconds(source.clip.length);
+            StartCoroutine(playVoiceLines());
+        }
+        //subtitleTexts.Clear();
+        //voiceOverLines.Clear();
+    }
+
+    public void PlayVoice()
+    {
+        if (currentLine < voiceOverLines.Count)
+        {
+            if (!source.isPlaying)
+            {
+                subtitles.text = subtitleTexts[currentLine];
+                source.clip = voiceOverLines[currentLine];
+                source.Play();
+
+                currentLine++;
+            }
+            else
+            {
+                
+            }
+        }
+    }
+}
--- Assets/Scripts/SubtitleDisplay.cs
+++ Assets/Scripts/SubtitleDisplay.cs
@@ -5,6 +5,8 @@ using TMPro;
 
 public class SubtitleDisplay : MonoBehaviour
 {
+    public DialogueManager manager;
+
     public TextMeshProUGUI subtitles;
     public AudioSource source;
 
@@ -18,6 +20,8 @@ public class SubtitleDisplay : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        manager = GameObject.Find("DialogueManager").GetComponent<DialogueManager>();
+
         subtitles = GameObject.Find("Subtitles").GetComponent<TextMeshProUGUI>();
         source = GameObject.Find("Subtitles").GetComponent<AudioSource>();
 
@@ -31,27 +35,45 @@ public class SubtitleDisplay : MonoBehaviour
         {
             subtitles.text = "";
         }
-    }
-
-    IEnumerator playVoiceLines()
-    {
-        subtitles.text = subtitleTexts[currentLine];
-        source.clip = voiceOverLines[currentLine];
-        source.Play();
-        if (currentLine < voiceOverLines.Length)
+        if (triggered)
         {
-            currentLine++;
-            yield return new WaitForSeconds(source.clip.length);
-            StartCoroutine(playVoiceLines());
+            manager.PlayVoice();
         }
     }
+
+    //IEnumerator playVoiceLines()
+    //{
+    //    subtitles.text = subtitleTexts[currentLine];
+    //    source.clip = voiceOverLines[currentLine];
+    //    source.Play();
+    //    if (currentLine < voiceOverLines.Length)
+    //    {
+    //        currentLine++;
+    //        yield return new WaitForSeconds(source.clip.length);
+    //        StartCoroutine(playVoiceLines());
+    //    }
+    //}
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.name == "Player" && !triggered)
         {
-            StartCoroutine(playVoiceLines());
+            manager.source.Stop();
+            manager.currentLine = 0;
             triggered = true;
+            manager.subtitleTexts.Clear();
+            manager.voiceOverLines.Clear();
+            manager.subtitleTexts.AddRange(subtitleTexts);
+            manager.voiceOverLines.AddRange(voiceOverLines);
+
+            if (manager.currentTrigger != null)
+            {
+                Destroy(manager.currentTrigger);
+            }
+            manager.currentTrigger = this.gameObject;
+
+            //StopCoroutine(manager.playVoiceLines());
+            //StartCoroutine(manager.playVoiceLines());
         }
     }
 }
