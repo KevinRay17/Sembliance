7dbf43e059b9823d6eefa30e9819e8557afe99e1
Samina Trachier <st3225@nyu.edu>
2019-11-05 13:41:43 -0500

    Added UI Features for flip (needs implimentation)

---
+++ Assets/Scripts/FlipUI.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FlipUI : MonoBehaviour
+{
+    //make the UI sprite of the arrow switch when in the correct state
+   //or if the player cannot jump the red versions appear
+
+    public Sprite blackArrow;
+
+    public Sprite whiteArrow;
+
+    public Sprite REDblackArrow;
+
+    public Sprite REDwhiteArrow;
+
+
+    void Start()
+    {
+        changeImages();
+    }
+
+    void changeImages() //attach to event trigger
+    {
+        if (CitySwap.WhiteUI == true)
+        {
+            gameObject.GetComponent<Image>().sprite = blackArrow;
+        }
+        if(CitySwap.WhiteUI == false)
+        {
+            Debug.Log("On black has returned true");
+            gameObject.GetComponent<Image>().sprite = whiteArrow;
+
+        }
+
+     
+    }
+}
